14c1a8eb2d925eee85481510019bfc91591dd86c
Hannidiot <hannidiot2018@gmail.com>
2025-08-30 10:22:18 +0800

    misc: add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 function App() {
-  return (
-    <h1 className="text-3xl font-bold text-red-500 text-center">App</h1>
-  )
+  return <h1 className="text-3xl font-bold text-red-500 text-center">App</h1>;
 }
 
-export default App
+export default App;
